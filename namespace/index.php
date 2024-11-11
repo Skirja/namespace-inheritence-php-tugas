b4ee8d16844b3d2d9115454bfa29409ff6827526
Skirja <irzafinmuh11@gmail.com>
2024-11-11 13:16:30 +0700

    update

--- namespace/index.php
+++ namespace/index.php
@@ -1,17 +1,13 @@
 <?php
 
-// Memuat file kelas
 require_once __DIR__ . '/Store/Product.php';
 require_once __DIR__ . '/Warehouse/Product.php';
 
-// Using both Product classes with aliases
 use Store\Product as StoreProduct;
 use Warehouse\Product as WarehouseProduct;
 
-// Create instances of both Product classes
 $storeProduct = new StoreProduct("Laptop", 999.99);
 $warehouseProduct = new WarehouseProduct("Laptop", 50);
 
-// Display information from both products
 echo $storeProduct->getInfo() . "\n";
 echo $warehouseProduct->getInfo() . "\n"; 
